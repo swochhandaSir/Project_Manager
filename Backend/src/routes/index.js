fae025b80848f7f22d2c809e99e6f476c783d54f
Swochhanda Jangam <swochhandajangam@gmail.com>
2026-03-29 09:08:09 +0545

    fixed import error in errorMiddleware"

--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -4,7 +4,7 @@ import userRoute from "./userRoute.js";
 import authRoute from "./authRoute.js";
 import taskRoute from "./taskRoute.js";
 import commentRoute from "./commentRoute.js";
-import { verifyToken } from "../middleware/verifyManager.js";
+import { verifyToken } from "../middleware/verifyToken.js";
 
 const routes = express.Router();
 routes.use("/project", verifyToken, projectRoute);
